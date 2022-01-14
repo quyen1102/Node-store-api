2bd79b3132c3bcb3e30adf22720d7f9ad8d0728f
quyen1102 <quyen.ttq.1102@gmail.com>
2022-01-14 23:52:49 +0700

    done

--- src/app/controller/productController.js
+++ src/app/controller/productController.js
@@ -6,7 +6,7 @@ const getAllProductStatic = async (req, res)=>{
    res.status(200).json({nbHits: products.length,products })
 }
 const getAllProduct = async (req, res) => {
-    const { featrued, company, name, sort, fields } =  req.query
+    const { featrued, company, name, sort, fields, numbericFilters } =  req.query
     const queryObject = {}
     
     
@@ -18,6 +18,27 @@ const getAllProduct = async (req, res) => {
     }
     if(name) {
         queryObject.name = { $regex: name, $options: 'i' } 
+    }
+    if(numbericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '<': '$lt',
+            '<=': '$lte',
+            '=': '$eq',
+        }
+        let regex = /\b(>|<|>=|<=|=)\b/g
+        let filters = numbericFilters.replace(regex, (match) => `-${operatorMap[match]}-`)
+        console.log(filters);
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach(filter => {
+            const [fields, operator, value] = filter.split('-')
+           
+            if(options.includes(fields)) {
+                queryObject[fields] = { [operator]: Number(value) }
+            }
+        })
+        console.log(queryObject)
     }
   
     let result =  Product.find(queryObject)
